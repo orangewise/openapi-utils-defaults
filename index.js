58a8ffac2a842cdf4eb4c9ec9b98d71557a6fef7
ronald (MacBookie Pro) <ronald@orangewise.com>
2017-01-09 21:10:38 +0100

    convert to es6 with lebab

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-var defaults = require('lodash.defaultsdeep')
-var RefParser = require('json-schema-ref-parser')
+const defaults = require('lodash.defaultsdeep')
+const RefParser = require('json-schema-ref-parser')
 
-exports.apiDefaults = function (target, source, cb) {
-  RefParser.dereference(target, function (e, t) {
+const apiDefaults = (target, source, cb) => {
+  RefParser.dereference(target, (e, t) => {
     if (e) return cb(e)
-    RefParser.dereference(source, function (e, s) {
+    RefParser.dereference(source, (e, s) => {
       if (e) return cb(e)
       return cb(null, defaults(t, s))
     })
   })
 }
+
+module.exports = { apiDefaults }
--- test/test-index.js
+++ test/test-index.js
@@ -1,38 +1,38 @@
-var test = require('tape')
-var defaults = require('../.')
-var join = require('path').join
+const test = require('tape')
+const defaults = require('../.')
+const join = require('path').join
 
-test('defaults 01', function (t) {
+test('defaults 01', t => {
   defaults.apiDefaults(
     join(__dirname, 'fixtures/01target.yaml'),
     join(__dirname, 'fixtures/01source.yaml'),
-    function (e, r) {
+    (e, r) => {
       t.equal(e, null, 'no errors found')
-      var expected = require(join(__dirname, 'fixtures/01result.json'))
+      const expected = require(join(__dirname, 'fixtures/01result.json'))
       t.deepEqual(r, expected, 'merge succeeded')
       t.end()
     })
 })
 
-test('defaults 02', function (t) {
+test('defaults 02', t => {
   defaults.apiDefaults(
     join(__dirname, 'fixtures/02target.yaml'),
     join(__dirname, 'fixtures/02source.yaml'),
-    function (e, r) {
+    (e, r) => {
       t.equal(e, null, 'no errors found')
-      var expected = require(join(__dirname, 'fixtures/02result.json'))
+      const expected = require(join(__dirname, 'fixtures/02result.json'))
       t.deepEqual(r, expected, 'merge succeeded')
       t.end()
     })
 })
 
-test('defaults 03', function (t) {
+test('defaults 03', t => {
   defaults.apiDefaults(
     join(__dirname, 'fixtures/03target.yaml'),
     join(__dirname, 'fixtures/03source.yaml'),
-    function (e, r) {
+    (e, r) => {
       t.equal(e, null, 'no errors found')
-      var expected = require(join(__dirname, 'fixtures/03result.json'))
+      const expected = require(join(__dirname, 'fixtures/03result.json'))
       t.deepEqual(r, expected, 'merge succeeded')
       t.end()
     })
