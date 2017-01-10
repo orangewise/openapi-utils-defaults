483823c5cb5417de55932674951a1fd2efed8277
ronald (MacBookie Pro) <ronald@orangewise.com>
2017-01-10 15:27:42 +0100

    added some tests

--- test/test-index.js
+++ test/test-index.js
@@ -2,7 +2,7 @@ var test = require('tape')
 var defaults = require('../.')
 var join = require('path').join
 
-test('defaults 01', function (t) {
+test('defaults 01: properties are added', function (t) {
   defaults.apiDefaults(
     join(__dirname, 'fixtures/01target.yaml'),
     join(__dirname, 'fixtures/01source.yaml'),
@@ -14,7 +14,7 @@ test('defaults 01', function (t) {
     })
 })
 
-test('defaults 02', function (t) {
+test('defaults 02: properties are added', function (t) {
   defaults.apiDefaults(
     join(__dirname, 'fixtures/02target.yaml'),
     join(__dirname, 'fixtures/02source.yaml'),
@@ -26,13 +26,25 @@ test('defaults 02', function (t) {
     })
 })
 
-test('defaults 03', function (t) {
+test('defaults 03: response is not overwritten', function (t) {
   defaults.apiDefaults(
     join(__dirname, 'fixtures/03target.yaml'),
     join(__dirname, 'fixtures/03source.yaml'),
     function (e, r) {
       t.equal(e, null, 'no errors found')
       var expected = require(join(__dirname, 'fixtures/03result.json'))
+      t.deepEqual(r, expected, 'merge succeeded')
+      t.end()
+    })
+})
+
+test('defaults 04: overwrite a nested key (method)', function (t) {
+  defaults.apiDefaults(
+    join(__dirname, 'fixtures/04target.yaml'),
+    join(__dirname, 'fixtures/04source.yaml'),
+    function (e, r) {
+      t.equal(e, null, 'no errors found')
+      var expected = require(join(__dirname, 'fixtures/04result.json'))
       t.deepEqual(r, expected, 'merge succeeded')
       t.end()
     })
